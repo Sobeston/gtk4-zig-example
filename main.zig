a21bbf886502327cb824b706a092364643626474
Sobeston <15335529+Sobeston@users.noreply.github.com>
2021-01-28 23:41:24 +0000

    initial

---
+++ main.zig
@@ -0,0 +1,25 @@
+const std = @import("std");
+const c = @cImport(@cInclude("gtk/gtk.h"));
+
+pub fn main() !void {
+    const app = c.gtk_application_new("org.gtk.example", .G_APPLICATION_FLAGS_NONE) orelse @panic("null app :(");
+    defer c.g_object_unref(app);
+
+    _ = c.g_signal_connect_data(
+        app,
+        "activate",
+        @ptrCast(c.GCallback, struct {
+            fn f(a: *c.GtkApplication, data: c.gpointer) void {
+                const window = c.gtk_application_window_new(a);
+                c.gtk_window_set_title(@ptrCast(*c.GtkWindow, window), "testing program");
+                c.gtk_window_set_default_size(@ptrCast(*c.GtkWindow, window), 400, 400);
+                c.gtk_widget_show(window);
+            }
+        }.f),
+        null,
+        null,
+        @intToEnum(c.GConnectFlags, 0),
+    );
+
+    _ = c.g_application_run(@ptrCast(*c.GApplication, app), 0, null);
+}
